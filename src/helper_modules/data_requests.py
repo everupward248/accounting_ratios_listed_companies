5d429d97488ba1a4aeaab5abd42c72a13e4a9c25
everupward248 <everupward248@gmail.com>
2025-09-06 11:32:49 -0500

    added functionality to obtain the company name or ticker based on ticker or company name. To add option to take company ticker or name and then obtain relevant data

--- src/helper_modules/data_requests.py
+++ src/helper_modules/data_requests.py
@@ -12,14 +12,15 @@ API_KEY = os.getenv("API_KEY")
 logger = get_logger(__name__)
 
 def main():
-    print(get_ticker())
+    print(get_name())
 
-def get_ticker():
+def get_name():
     """
-    returns the ticker for the company name provided
+    returns the company name for the ticker name provided
+
     """
     while True:
-        name = input("Please provide a valid company name or ticker: ")
+        name = input("Please provide a valid company ticker: ")
 
         company_name = name.strip()
         parameters = {
@@ -29,27 +30,74 @@ def get_ticker():
         
         try:
             response = requests.get(f"{BASE_URL}/search-symbol", params=parameters)
-            print(response.url)
-            print(response.status_code)
 
             if response.status_code == 200:
-                logger.info(f"Request successful for request at url: {response.url}")
+                logger.info(f"Request successful, status code: {response.status_code} for request at url: {response.url}")
                 shared_logger.info(f"Request successful for request at url: {response.url}")
                 response = response.json()
                 
                 if not response:
-                    print("Please provide a valid company\n")
+                    print("Invalid ticker provided. Please ensure this is the company's valid ticker.")
                     continue
+                # data returned is for all exchanges, obtain the ticker for the US exchange
+                for x in response:
+                    if x["currency"] == "USD":
+                        company_name = str(x["name"])
+                    else:
+                        pass
             else:
                 response.raise_for_status()
-
-            return response
-
+            return company_name
+        except requests.HTTPError:
+            logger.warning(requests.HTTPError)
+            shared_logger.warning(requests.HTTPError)
         except Exception as e:
             logger.warning(f"response request has failed, status code: {response.status_code}")
             shared_logger.warning(f"response request has failed, status code: {response.status_code}")
             print(e)
 
+def get_ticker() -> str:
+    """
+    returns the ticker for the company name provided
+
+    """
+    while True:
+        ticker = input("Please provide a valid, legally registered company name: ")
+
+        company_ticker = ticker.strip()
+        parameters = {
+            "query": company_ticker,
+            "apikey": API_KEY
+        }
+        
+        try:
+            response = requests.get(f"{BASE_URL}/search-name", params=parameters)
+
+            if response.status_code == 200:
+                logger.info(f"Request successful, status code: {response.status_code} for request at url: {response.url}")
+                shared_logger.info(f"Request successful for request at url: {response.url}")
+                response = response.json()
+                
+                if not response:
+                    print("Invalid company name provided. Please ensure that the name provided is the company's legally registered name.")
+                    continue
+
+                # data returned is for all exchanges, obtain the ticker for the US exchange
+                for x in response:
+                    if x["currency"] == "USD":
+                        ticker = str(x["symbol"])
+                    else:
+                        pass
+            else:
+                response.raise_for_status()
+            return ticker
+        except requests.HTTPError:
+            logger.warning(requests.HTTPError)
+            shared_logger.warning(requests.HTTPError)
+        except Exception as e:
+            logger.warning(f"response request has failed, status code: {response.status_code}")
+            shared_logger.warning(f"response request has failed, status code: {response.status_code}")
+            print(e)
 
 def get_financials():
     ...
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from src.helper_modules.logger_setup import get_logger, shared_logger
 from src.helper_modules import accounting_ratios as ar
+from src.helper_modules import data_requests as dr
 import argparse
 
 
@@ -9,14 +10,16 @@ def main() -> None:
     parser = cli()
     args = parser.parse_args()
 
-    
-    logger.info("separate log file")
-    shared_logger.info("main entry point")
-    ar.logger_test()
+    if args.get_name_ticker == "name":
+        print(dr.get_ticker())
+    elif args.get_name_ticker == "ticker":
+        print(dr.get_name())
     
 
 def cli() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="This program is a CLI tool which allows users to fetch the accounting ratios of listed companies when provided the ticker.")
+    parser.add_argument("-nt", "--name_ticker", action="store_true", help="Omit this flag for tickers and include for the legally registered company name.")
+    parser.add_argument("-gnt", "--get_name_ticker", type=str, choices=["name", "ticker"], help="Use this flag to obtain the legally registered company name or ticker per the ticker and company name respectively.")
     return parser
 
 if __name__ == "__main__":
