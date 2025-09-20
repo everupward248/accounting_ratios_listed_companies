90b830c6420ccf5de7ba75ba13918b1d9f61da62
everupward248 <everupward248@gmail.com>
2025-09-20 13:15:33 -0500

    sileced the type checker warning from passing the dataframe into tabulate. to implement the accounting ratio logic

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from src.helper_modules import accounting_ratios as ar
 from src.helper_modules import data_requests as dr
 from tabulate import tabulate
 import argparse
+import pandas as pd
 
 
 def main() -> None:
@@ -16,15 +17,34 @@ def main() -> None:
     elif args.get_name_ticker == "ticker":
         print(dr.get_name())
 
+    # .tolist() used as if the dataframe is passed directly to tabulate() the type checker will raise a warning though the function still executes without issue
     if args.balance_sheet:
-        if (df := dr.get_bs(args.balance_sheet)) is not None and not df.empty: 
-            print(tabulate(df, tablefmt="grid"))
+        if (df := dr.get_bs(args.balance_sheet)) is not None and not df.empty:
+            print(tabulate(df.reset_index().values.tolist(), tablefmt="grid"))
+            logger.info(f"The balance sheet data for the requested company has been successfully returned to the user: {df[0]["symbol"]}")
+            shared_logger.info(f"The balance sheet data for the requested company has been successfully returned to the user: {df[0]["symbol"]}")
+        else:
+            print("There was an issue rendering your request. Please try again.")
+            logger.warning(f"Function returned: {df}")
+            shared_logger.warning(f"Function returned: {df}")
     elif args.income_statement:
         if (df := dr.get_is(args.income_statement)) is not None and not df.empty: 
-            print(tabulate(df, tablefmt="grid"))
+            print(tabulate(df.reset_index().values.tolist(), tablefmt="grid"))
+            logger.info(f"The income statement data for the requested company has been successfully returned to the user: {df[0]["symbol"]}")
+            shared_logger.info(f"The income statement data for the requested company has been successfully returned to the user: {df[0]["symbol"]}")
+        else:
+            print("There was an issue rendering your request. Please try again.")
+            logger.warning(f"Function returned: {df}")
+            shared_logger.warning(f"Function returned: {df}")
     elif args.cash_flows:
-         if (df := dr.get_cf(args.cash_flows)) is not None and not df.empty: 
-            print(tabulate(df, tablefmt="grid"))
+        if (df := dr.get_cf(args.cash_flows)) is not None and not df.empty: 
+            print(tabulate(df.reset_index().values.tolist(), tablefmt="grid"))
+            logger.info(f"The cash flows data for the requested company has been successfully returned to the user: {df[0]["symbol"]}")
+            shared_logger.info(f"The cash flows data for the requested company has been successfully returned to the user: {df[0]["symbol"]}")
+        else:
+            print("There was an issue rendering your request. Please try again.")
+            logger.warning(f"Function returned: {df}")
+            shared_logger.warning(f"Function returned: {df}")
     
 
 def cli() -> argparse.ArgumentParser:
